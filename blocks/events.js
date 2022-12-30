2c5c07d1b2b2b7faedbf6d015d72ca044b6396ae
Cipher-Client <Pringlecpp@gmail.com>
2022-12-30 18:52:34 -0500

    Lua generator fix

--- blocks/events.js
+++ blocks/events.js
@@ -16,7 +16,9 @@ Blockly.Blocks['event_server_startup'] = {
 };
 
 window.Generator['event_server_startup'] = function(block) {
-    return "-- Generated using Ludum Visual Script\n";
+    var statements_statement = window.Generator.statementToCode(block, 'CODE');
+    var code = '-- Generated using Ludum Visual Script\n' + statements_statement + '\n';
+    return code
 };
 
 Blockly.Blocks['event_player_join'] = {
@@ -34,18 +36,11 @@ Blockly.Blocks['event_player_join'] = {
     }
 };
 
-// Uncaught TypeError: Cannot read properties of undefined (reading 'nameDB_')
-//     at window.Generator.event_player_join (events.js:38:39)
-//     at r.CodeGenerator$$module$build$src$core$generator.blockToCode (generator.ts:233:21)
-//     at r.CodeGenerator$$module$build$src$core$generator.workspaceToCode (generator.ts:125:23)
-//     at document.getElementById.onclick (workspace.js:409:31)
-
 window.Generator['event_player_join'] = function(block) {
-    var variable_player = Blockly.Lua.nameDB_.getName(block.getFieldValue('PLAYER'), Blockly.Variables.NAME_TYPE);
-    var statements_statement = Blockly.Lua.statementToCode(block, 'STATEMENT');
-    // TODO: Assemble Lua into code variable.
-    var code = '...\n';
-    return code;
+    var variable_player = window.Generator.nameDB_.getName(block.getFieldValue('PLAYER'), Blockly.Variables.NAME_TYPE);
+    var statements_statement = window.Generator.statementToCode(block, 'STATEMENT');
+    var code = 'game.Players.PlayerAdded:Connect(function(player)\n' + statements_statement + 'end)\n';
+    return code
 };
 
 Blockly.Blocks['event_player_leave'] = {
