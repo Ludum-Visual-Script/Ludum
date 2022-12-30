3d7b85cda1de8b41f0d8a8040ed0066e6b2087dd
Jeremiah <46662314+UCYT5040@users.noreply.github.com>
2022-12-29 19:55:42 -0600

    Remove some useless comments & unused example extension

--- workspace.js
+++ workspace.js
@@ -381,70 +381,12 @@ function loadExtension(extension) {
     workspace.refreshToolboxSelection();
 }
 
-// an extension called "Hello World" with two categories:
-// one has a block which prints "Hello World" to the console
-// the other has a block which prints "Goodbye World" to the console
-let HelloWorld = {
-    "name": "Hello World", // Users will see this name in the extension manager
-    "description": "A simple extension that add blocks which print \"Hello World\" or \"Goodbye World\" to the console.", // Also shown in the extension manager
-    "version": "1.0.0", // Must be in format Major.Minor.Patch (see https://semver.org/)
-    "ludumVersion": "1.0.0", // The version of Ludum this extension was made for
-    "categories": [ // The categories that the extension adds to the left toolbox
-        {
-            "name": "Hello", // The name of the category
-            "blocks": [ // The blocks that the category contains
-                {
-                    "type": "hello_world", // The internal name of the block
-                    "block": { // The block definition (see https://developers.google.com/blockly/guides/create-custom-blocks/define-blocks)
-                        "init": function() {
-                            this.appendDummyInput()
-                                .appendField("Hello World");
-                            this.setPreviousStatement(true, null);
-                            this.setNextStatement(true, null);
-                            this.setColour(230);
-                            this.setTooltip("Prints \"Hello World\" to the console.");
-                            this.setHelpUrl(""); // Optional, remove if not needed
-                        }
-                    },
-                    "generator": function(block) { // The Lua generator (see https://developers.google.com/blockly/guides/create-custom-blocks/generating-code)
-                        return "print(\"Hello World\")\n";
-                    }
-                }
-            ]
-        },
-        {
-            "name": "Goodbye",
-            "blocks": [
-                {
-                    "type": "goodbye_world",
-                    "block": {
-                        "init": function() {
-                            this.appendDummyInput()
-                                .appendField("Goodbye World");
-                            this.setPreviousStatement(true, null);
-                            this.setNextStatement(true, null);
-                            this.setColour(230);
-                            this.setTooltip("Prints \"Goodbye World\" to the console.");
-                            this.setHelpUrl("");
-                        }
-                    },
-                    "generator": function(block) {
-                        return "print(\"Goodbye World\")\n";
-                    }
-                }
-            ]
-        }
-    ]
-}
 
-inputDiv = document.getElementById("input");
+let inputDiv = document.getElementById("input");
 document.getElementById("extensions").onclick = () => {
-    // show the extensions input
     inputDiv.style.display = "block";
 }
 document.getElementById("load").onclick = () => {
-    // get the input eval and load
-    // also hide the input
     inputDiv.style.display = "none";
     let extension = eval(`(${document.getElementById("inputTextarea").value})`);
     console.log(extension);
