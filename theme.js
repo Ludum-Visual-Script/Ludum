7a7a55fbdc1ec962a09981cae24960c40236e8f0
Cipher-Client <Pringlecpp@gmail.com>
2023-02-24 10:26:15 -0500

    Revert "Website upload"
    This reverts commit d06e60330b09251187faf7e32a251cadd0024d63.

---
+++ theme.js
@@ -0,0 +1,93 @@
+import Blockly from 'blockly/core';
+
+export default Blockly.Theme.defineTheme('ludum', {
+    'base': Blockly.Themes.Classic,
+    'blockStyles': {
+        'colour_blocks': {
+            'colourPrimary': '#a5745b',
+            'colourSecondary': '#dbc7bd',
+            'colourTertiary': '#845d49',
+        },
+        'list_blocks': {
+            'colourPrimary': '#7569f0',
+            'colourSecondary': '#aba3ff',
+            'colourTertiary': '#292366',
+        },
+        'logic_blocks': {
+            'colourPrimary': '#ffc21c',
+            'colourSecondary': '#ffdb78',
+            'colourTertiary': '#967d38',
+        },
+        'loop_blocks': {
+            'colourPrimary': '#ffc21c',
+            'colourSecondary': '#ffdb78',
+            'colourTertiary': '#967d38',
+        },
+        'math_blocks': {
+            'colourPrimary': '#76e05c',
+            'colourSecondary': '#b5ffa3',
+            'colourTertiary': '#347325',
+        },
+        'procedure_blocks': {
+            'colourPrimary': '#e01b1b',
+            'colourSecondary': '#ff8282',
+            'colourTertiary': '#5c0c0c',
+        },
+        'text_blocks': {
+            'colourPrimary': '#27c4b4',
+            'colourSecondary': '#69fff0',
+            'colourTertiary': '#147067',
+        },
+        'variable_blocks': {
+            'colourPrimary': '#cc45ca',
+            'colourSecondary': '#f797f6',
+            'colourTertiary': '#691e68',
+        },
+        'variable_dynamic_blocks': {
+            'colourPrimary': '#cc45ca',
+            'colourSecondary': '#f797f6',
+            'colourTertiary': '#691e68',
+        },
+        'hat_blocks': {
+            'colourPrimary': '#a55b99',
+            'colourSecondary': '#dbbdd6',
+            'colourTertiary': '#84497a',
+            'hat': 'cap',
+        },
+    },
+    'categoryStyles': {
+        'events_category': {
+            'colour': '#7500FF',
+        },
+        'statements_category': {
+            'colour': '#ffc21c',
+        },
+        'operators_category': {
+            'colour': '#76e05c',
+        },
+        'strings_category': {
+            'colour': '#27c4b4',
+        },
+        'tables_category': {
+            'colour': '#7569f0',
+        },
+        'variables_category': {
+            'colour': '#cc45ca',
+        },
+        'funtions_category': {
+            'colour': '#e01b1b',
+        },
+        'extensions_category': {
+            'colour': '#e01b1b',
+        },
+    },
+    'componentStyles': {
+        'workspaceBackgroundColour': '#1e1e1e',
+        'toolboxBackgroundColour': '#1e1e1e',
+        'toolboxForegroundColour': '#ffffff',
+        'flyoutBackgroundColour': '#2b2b2b',
+        'flyoutForegroundColour': '#ffffff',
+    },
+    'fontStyle': {},
+    'startHats': null,
+});
